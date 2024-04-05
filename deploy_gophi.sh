e1b23964111817ff2149983c928aae74e2de0b4e
alpensys <rtauxe@gmail.com>
2024-04-05 10:09:01 +0200

    202404051009

--- deploy_gophi.sh
+++ deploy_gophi.sh
@@ -7,7 +7,7 @@
 #   Everything made on this script are made for educational purposes              #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-echo "Welcome to this automated script to deploy a complete installation of GoPhish framework. It make it quick & easy."
+echo -e "\nWelcome to this automated script to deploy a complete installation of GoPhish framework. It make it quick & easy."
 
 # Find the current location
 
@@ -20,21 +20,26 @@ defname="cas-cyber-""$randomstring"
 
 # Create the server in the Openstack environment
 
-echo -e "Server creation has been requested. Please wait..."
+echo -e "\nServer creation has been requested. Please wait..."
 
-openstack server create --image "Ubuntu 22.04 LTS Jammy Jellyfish" --flavor a2-ram4-disk20-perf1 --key-name rt-rsa --network ext-net1 $defname --wait
+openstack server create --image "Ubuntu 22.04 LTS Jammy Jellyfish" --flavor a2-ram4-disk20-perf1 --key-name rt-rsa --network ext-net1 $defname --wait > /dev/null 2>&1
+
+echo -e "Server created with name $defname"
 
 # Getting the IP address of the server. "jq" package must be installed.
 
-ipadd=$(openstack server show $defname -f json | jq -r '.addresses'| grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+ipadd=$(openstack server show $defname -f json | jq -r '.addresses'| grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b") 
 
 # Creation of a A record on the delegated DNS zone
+echo -e "\nCreation of an A record on the delegated DNS zone for FQDN $randomstring.cloud.rt-cas-cyber.ch"
 
-openstack recordset create --type A --record $ipadd cloud.rt-cas-cyber.ch. $randomstring
-
+openstack recordset create --type A --record $ipadd cloud.rt-cas-cyber.ch. $randomstring > /dev/null 2>&1
 sleep 15
 
+echo "DNS record created"
+
 # Some echo to give informations about the current server
+# The first echo will overwrite the existing file
 
 echo -e "The server name is : $defname" > server_info.txt
 echo -e "The admin URL of Gophish is : https://$randomstring.cloud.rt-cas-cyber.ch:3333/" >> server_info.txt
@@ -42,18 +47,23 @@ echo -e "To connect with SSH on the server, please use : ssh ubuntu@$ipadd with 
 
 # Copy of the config file on the destination server 
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$current_location"/config.json ubuntu@$ipadd:/home/ubuntu/config.json
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$current_location"/config.json ubuntu@$ipadd:/home/ubuntu/config.json > /dev/null 2>&1
 
 # Array of commands that will be run on the server. These commands will deploy GoPhish.
 
+echo -e "\nInstallation and configuration of Gophish on destination server with IP $ipadd"
+echo -e "Once done, current output will display GoPhish logs and provide username and password. Please refer to server_info.txt file to get admin URL.\n"
+
+sleep 5
+
 commands=(
-        "sudo apt install unzip -y"
-        "mkdir /home/ubuntu/server"
-        "cd /home/ubuntu/server && wget https://github.com/gophish/gophish/releases/download/v0.12.1/gophish-v0.12.1-linux-64bit.zip"
-        "sudo unzip /home/ubuntu/server/gophish-v0.12.1-linux-64bit.zip -d /home/ubuntu/server/"
-        "sudo chmod +x /home/ubuntu/server/gophish"
-        "sudo rm /home/ubuntu/server/config.json && sudo cp /home/ubuntu/config.json /home/ubuntu/server/"
-        "cd /home/ubuntu/server && sudo ./gophish"
+        "sudo apt install unzip -y > /dev/null 2>&1"
+        "mkdir /home/ubuntu/server > /dev/null 2>&1"
+        "cd /home/ubuntu/server && wget https://github.com/gophish/gophish/releases/download/v0.12.1/gophish-v0.12.1-linux-64bit.zip > /dev/null 2>&1"
+        "sudo unzip /home/ubuntu/server/gophish-v0.12.1-linux-64bit.zip -d /home/ubuntu/server/ > /dev/null 2>&1"
+        "sudo chmod +x /home/ubuntu/server/gophish > /dev/null 2>&1"
+        "sudo rm /home/ubuntu/server/config.json && sudo cp /home/ubuntu/config.json /home/ubuntu/server/ > /dev/null 2>&1"
+        "cd /home/ubuntu/server && sudo ./gophish > gophish.log"
         )
 
         #Il reste à générer un certificat
@@ -62,5 +72,5 @@ commands=(
 
 for command in "${commands[@]}"; do
         ssh ubuntu@$ipadd -o StrictHostKeyChecking=no "$command"
-        sleep 1
+        sleep 2
 done
