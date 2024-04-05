27b194cf5718b82f852f880fb29a3bf2fdfaf400
alpensys <rtauxe@gmail.com>
2024-04-05 10:25:55 +0200

    202404051026

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# Script permettant de déployer une instance de GoPhish facilement
+# Script permettant de déployer une instance de GoPhish de manière automatisée
 
 Le phishing, on connait, on a deja vu, etc...
 La meilleur manière de se protéger s'est en formant et en sensibilisant
-J'entends des fois : Oui mais mettre en place une infrastructure de phishing c'est pas facile
+J'entends des fois : Oui mais mettre en place une infrastructure de phishing c'est pas facile.
 
-A faire mtn sur le script :
+Voici la solution, un script qui automatise le déploiement et la configuration de base de GoPhish sur un serveur dans le Public Cloud d'Infomaniak.
 
-- Modification de la config GoPhish
+## Améliorations attendues
+
 - Passage en HTTPs GoPhish avec certificat signé
-- Lancement de GoPhish en background
-- Récupération du login admin par le script
 
 ## Documentation
 
--- deploy_gophi.sh
+++ deploy_gophi.sh
@@ -10,24 +10,22 @@
 echo -e "\nWelcome to this automated script to deploy a complete installation of GoPhish framework. It make it quick & easy."
 
 # Find the current location
-
 current_location="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Define the server name by using a random function
-
 randomstring=$(printf '%s' $(echo "$RANDOM" | md5sum) | cut -c 1-10)
 defname="cas-cyber-""$randomstring"
 
 # Create the server in the Openstack environment
-
 echo -e "\nServer creation has been requested. Please wait..."
-
 openstack server create --image "Ubuntu 22.04 LTS Jammy Jellyfish" --flavor a2-ram4-disk20-perf1 --key-name rt-rsa --network ext-net1 $defname --wait > /dev/null 2>&1
 
-echo -e "Server created with name $defname"
+# Getting the IP address of the server. "jq" package must be installed.
+ipadd=$(openstack server show $defname -f json | jq -r '.addresses'| grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b") 
+
+echo -e "Server created with name $defname and IP $ipadd"
 
 # Getting the IP address of the server. "jq" package must be installed.
-
 ipadd=$(openstack server show $defname -f json | jq -r '.addresses'| grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b") 
 
 # Creation of a A record on the delegated DNS zone
@@ -38,21 +36,21 @@ sleep 15
 
 echo "DNS record created"
 
+ssh-keygen -f "/home/rt/.ssh/known_hosts" -R "$ipadd"
+
 # Some echo to give informations about the current server
 # The first echo will overwrite the existing file
-
 echo -e "The server name is : $defname" > server_info.txt
 echo -e "The admin URL of Gophish is : https://$randomstring.cloud.rt-cas-cyber.ch:3333/" >> server_info.txt
 echo -e "To connect with SSH on the server, please use : ssh ubuntu@$ipadd with the certificate used on Openstack" >> server_info.txt
 
 # Copy of the config file on the destination server 
-
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$current_location"/config.json ubuntu@$ipadd:/home/ubuntu/config.json > /dev/null 2>&1
 
 # Array of commands that will be run on the server. These commands will deploy GoPhish.
-
 echo -e "\nInstallation and configuration of Gophish on destination server with IP $ipadd"
-echo -e "Once done, current output will display GoPhish logs and provide username and password. Please refer to server_info.txt file to get admin URL.\n"
+echo -e "Once done, current output will display GoPhish logs and provide username and password. Please refer to server_info.txt file to get admin URL."
+echo -e "Please note that when you close or CTRL+C this terminal, GoPhish server will stop. You'll be still able to SSH to it and run it manually.\n"
 
 sleep 5
 
@@ -69,8 +67,7 @@ commands=(
         #Il reste à générer un certificat
 
 # For boucle to execute the commands defined above
-
 for command in "${commands[@]}"; do
         ssh ubuntu@$ipadd -o StrictHostKeyChecking=no "$command"
-        sleep 2
+        sleep 3
 done
